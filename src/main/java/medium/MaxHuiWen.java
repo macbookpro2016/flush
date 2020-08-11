d7dfbd6904dc862f18c22ea10377fe8ad1e61484
冯 志航 <fengzhihang@mskj.com>
2020-08-11 16:59:54 +0800

    最长回文

--- src/main/java/medium/MaxHuiWen.java
+++ src/main/java/medium/MaxHuiWen.java
@@ -22,6 +22,8 @@ package medium;
  * 当剩余的长度小于已存在的最大回文长度时，停止
  * 每次循环时若已经小于等于当前最大长度时，停止
  * 若没有回文则返回第一个字符
+ *
+ * at last 使用中心扩散法或者动态规划法即可，我的思路相当于是两边向中间扩散
  */
 public class MaxHuiWen {
 
@@ -69,5 +71,45 @@ public class MaxHuiWen {
     public static void main(String[] args) {
         String s = solution("aaaa");
         System.out.println(s);
+    }
+
+    // 动态规划
+    public String longestPalindrome(String s) {
+        // 特判
+        int len = s.length();
+        if (len < 2) {
+            return s;
+        }
+
+        int maxLen = 1;
+        int begin = 0;
+
+        // dp[i][j] 表示 s[i, j] 是否是回文串
+        boolean[][] dp = new boolean[len][len];
+        char[] charArray = s.toCharArray();
+
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+        }
+        for (int j = 1; j < len; j++) {
+            for (int i = 0; i < j; i++) {
+                if (charArray[i] != charArray[j]) {
+                    dp[i][j] = false;
+                } else {
+                    if (j - i < 3) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                }
+
+                // 只要 dp[i][j] == true 成立，就表示子串 s[i..j] 是回文，此时记录回文长度和起始位置
+                if (dp[i][j] && j - i + 1 > maxLen) {
+                    maxLen = j - i + 1;
+                    begin = i;
+                }
+            }
+        }
+        return s.substring(begin, begin + maxLen);
     }
 }
